7ad50dd68122e78102368044b2896e0fecd4d0eb
OmarGabr0 <omargabr90@gmail.com>
2026-04-08 10:58:24 +0200

    fix: fix database connectio and registration

--- src/main/java/com/mycompany/twilio/RegisterServlet.java
+++ src/main/java/com/mycompany/twilio/RegisterServlet.java
@@ -9,6 +9,8 @@ import java.sql.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import java.util.Set;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Web application lifecycle listener.
@@ -18,29 +20,27 @@ import java.util.Set;
 public class RegisterServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         // if logged in
         HttpSession session = request.getSession(false);
-        if (session != null && session.getAttribute("isLoggedIn") == "yes") {
+        if (session != null && "yes".equals(session.getAttribute("isLoggedIn"))) {
             response.sendRedirect("Profile.html");
             return;
         }
-
-        response.sendRedirect("Register.html");
-    }
-
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-// create session 
-        HttpSession session = request.getSession(true);
+        //create the session
+        session = request.getSession(true);
 
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         String fullName = request.getParameter("fullName");
-        String birthDate = request.getParameter("birthday");
+        String birthDatestr = request.getParameter("birthday");
+        //convert to local date 
+        LocalDate localDate = LocalDate.parse(birthDatestr, DateTimeFormatter.ISO_DATE);
+        // convert to java sql date 
+        Date sqlDate = Date.valueOf(localDate);
+
         String job = request.getParameter("job");
         String password = request.getParameter("password");
 
@@ -58,27 +58,27 @@ public class RegisterServlet extends HttpServlet {
             //getting the connection using servlet context 
             ServletContext ctx = getServletContext();
             Connection con = (Connection) ctx.getAttribute("DBConnection");
-            
+
             PreparedStatement ps = con.prepareStatement("INSERT INTO users "
                     + "(full_name,birthday,job,email,password_hash,is_admin,msisdn,physical_address,"
                     + "twilio_account_sid,twilio_auth_token,twilio_sender_id,created_at,is_verified )"
                     + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP, FALSE)");
 
             ps.setString(1, fullName);
-            ps.setString(2, birthDate);
+            ps.setDate(2, sqlDate);
             ps.setString(3, job);
             ps.setString(4, email);
 
             ps.setString(5, password);
-            String is_admin = "0"; // by defualt any user is not an admine 
+            Boolean is_admin = false; // by defualt any user is not an admine 
 
-            ps.setString(6, is_admin);
-            ps.setString(5, msisdn);
-            ps.setString(5, physicalAddress);
+            ps.setBoolean(6, is_admin);
+            ps.setString(7, msisdn);
+            ps.setString(8, physicalAddress);
 
-            ps.setString(5, twilioSid);
-            ps.setString(5, twilioToken);
-            ps.setString(5, twilioSender);
+            ps.setString(9, twilioSid);
+            ps.setString(10, twilioToken);
+            ps.setString(11, twilioSender);
             //ps.setString(5, currentTimeStamp); is set defualt = current time stamp 
             // is verified value is false by defualt 
 
@@ -90,7 +90,7 @@ public class RegisterServlet extends HttpServlet {
 //                out.println("</body></html>");
 //            }
 
-         //   response.sendRedirect("VerifyServlet");
+            //   response.sendRedirect("VerifyServlet");
             request.getRequestDispatcher("OTPgenerationServlet").forward(request, response);
             con.close();
         } catch (Exception e) {
